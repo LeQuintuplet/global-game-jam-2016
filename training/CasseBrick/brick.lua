532de1d575a64dc94496dfd8c9b55494e3f9108e
EnzDev <enzo@mallard.ml>
2016-01-13 13:07:12 +0100

    Untested new function in main engine

--- training/CasseBrick/brick.lua
+++ training/CasseBrick/brick.lua
@@ -1,11 +1,17 @@
 --- Brick Game Engine
 -- @author Enzo Mallard
 local Bricks = {}
+local me = require("mapEngine")
 
 --[[ GAME OBJECT ]]-------------------------------------------------------------
-Bricks.Game = {bricks = {}}
+Bricks.Game = {bricks = {}, tileworld, tileset}
 
-function Bricks.Game(map)
+--- Initialize new game
+-- @param map is the localtion of the map file
+-- @param tileSet is the location of the tileset to use
+-- @param tilseSize is the width/height of one tile
+function Bricks.Game(map, tileSet, tileSize)
+    tileSize = tileSize or 32
     local locmap = require(map)
     setmetatable({}, Game)
     dataUnformated = locmap["layers"][1]["data"]
@@ -19,18 +25,44 @@ function Bricks.Game(map)
             n=n+1
         end
     end
-    self.bricks = setMap
+
+    self.bricks    = setMap
+    self.tileworld = me.Map:new(map)
+    self.tileset   = me.TileSet:new(tileSet, tileSize, tileSize)
     return self
 end
 
+--[[ PLATFORM OBJECT ]]---------------------------------------------------------
+Bricks.Platform = {x=48, h=32, l=96}
+function Bricks.Platform:new(xCoord, height, length)
+    setmetatable({}, Platform)
 
+    self.x = xCoord
+    self.h = height
+    self.l = length
+    return self
+end
+
+function Bricks.Platform:move()
+  x = love.mouse.getX()
+  if x<(self.l/2) then
+    self.x = 48
+  elseif x>love.graphics.getWidth()-(self.l/2) then
+    self.x = love.graphics.getWidth()-(self.l/2)
+  else
+    self.x = x
+  end
+end
+
+function Bricks.Platform:
+--[[ BALL OBJECT ]]-------------------------------------------------------------
 --[[ BRICK OBJECT ]]------------------------------------------------------------
 Bricks.Brick = {top = 0, left = 0, blockID=0}
 -- Height and Width is always 32
 
 function Bricks.Brick:new(top, left, id)
-    setmetatable({}, brick)
-
+    setmetatable({}, Brick)
+    id = id or 0
     self.top  = top
     self.left = left
     self.id   = id
