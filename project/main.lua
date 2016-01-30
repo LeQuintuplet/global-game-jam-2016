a0f19464fb8b19cf72b88ca96cfb21680f8da809
reiisteel <william.flambard@etu.univ-nantes.fr>
2016-01-30 15:03:18 +0100

    main change, add intro state

--- project/main.lua
+++ project/main.lua
@@ -3,16 +3,17 @@ Gamestate = require "libs.hump.gamestate"
 
 -- States
 local game = require "src.states.game"
+local intro = require "src.states.intro"
 
 function love.load()
 	Gamestate.registerEvents()
-	Gamestate.switch(game)
+	Gamestate.switch(intro)
 end
 
 function love.update(dt)
-	
+
 end
 
 function love.draw()
-	
+
 end
---
+++ project/src/states/intro.lua
@@ -0,0 +1,13 @@
+local intro = {} -- module start
+
+function intro:enter()
+	print("Current state : intro")
+end
+
+function intro:update(dt)
+end
+
+function intro:draw()
+end
+
+return intro -- module end
